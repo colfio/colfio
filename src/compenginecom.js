372bfe2556205a16f96d9db1f7895eb57682f77f
Adam Vesecký <vesecky.adam@gmail.com>
2018-05-04 14:41:00 +0200

    Add debug component

--- src/compenginecom.js
+++ src/compenginecom.js
@@ -1,3 +1,91 @@
+// Debugging component that renders the whole scene graph
+class DebugComponent extends Component {
+	
+	constructor(targetHtmlElement){
+		super();
+		this.targetHtmlElement = targetHtmlElement; // TODO add something more generic here
+		this.strWrapper = { str : ""};
+	}
+	
+	oninit() {
+		if(this.owner.parent != null){
+			throw new Error("DebugComponent must be attached to the very root!");
+		}
+
+		let originalDraw = this.scene.draw;
+		var self = this;
+		
+		// todo draw method is replaced which is not a good approach
+	/*	this.scene.draw = () => {
+			for (let gameObject of self.scene.sortedObjects) {
+				gameObject.draw(self.scene.canvasCtx);
+			}
+
+			let strokeStyle = self.scene.canvasCtx.strokeStyle;
+			self.scene.canvasCtx.beginPath();
+			self.scene.canvasCtx.strokeStyle = "red";
+			self._drawBoundingBox(self.scene.canvasCtx, self.owner);
+			self.scene.canvasCtx.stroke();
+			self.scene.canvasCtx.strokeStyle = strokeStyle;
+		};*/
+
+	}
+
+	update(delta, absolute){
+		this.strWrapper.str = "";
+		this._processNode(this.owner, this.strWrapper);	
+		this.targetHtmlElement.innerHTML = this.strWrapper.str;
+	}
+
+
+	_drawBoundingBox(ctx, node) {
+		let bb = node.mesh.bbox;
+		let posX = bb.topLeftX * this.scene.unitSize;
+		let posY = bb.topLeftY * this.scene.unitSize;
+		let size = bb.getSize();
+		
+		if(size.width != 0 && size.height != 0){
+			ctx.rect(posX, posY, size.width, size.height);
+		}
+
+		for(let [id, child] of node.children) {
+			this._drawBoundingBox(ctx,child);	
+		}
+	}
+
+	_setPadding(padding){
+		let otp = "";
+		for(let i=0; i<padding; i++){
+			otp = otp.concat("&nbsp");
+		}
+		return otp;
+	}
+
+	_processNode(node, strWrapper, padding = 0) {
+
+		// transform:
+		strWrapper.str += "<strong><span style=\"color:red\">";
+		strWrapper.str = strWrapper.str.concat(this._setPadding(padding+2) 
+		+`rel:[${node.trans.posX.toFixed(2)},${node.trans.posY.toFixed(2)}]|abs:[${node.trans.absPosX.toFixed(2)},${node.trans.absPosY.toFixed(2)}]|rot: ${node.trans.rotation.toFixed(2)}|z: ${node.zIndex}` 
+		+"<br>");
+		strWrapper.str += "</span></strong>";
+
+		for(let cmp of node.components) {
+			strWrapper.str += "<span style=\"color:blue\">";
+			strWrapper.str = strWrapper.str.concat(this._setPadding(padding+2) + cmp.constructor.name+"<br>");
+			strWrapper.str += "</span>";
+		}
+		
+		for(let [id, child] of node.children) {
+			strWrapper.str += "<span style=\"color:green\">";
+			strWrapper.str = strWrapper.str.concat(this._setPadding(padding) 
+			+ `${child.id}:${child.tag}`+"<br>");
+			this._processNode(child, strWrapper, padding+4);
+			strWrapper.str += "</span>";
+		}
+	}
+}
+
 // Rendering component that can render any mesh
 class BasicRenderer extends Component {
 
